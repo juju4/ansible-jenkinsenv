9fc46b3713a0854bb9572d2bfaf347d33974f040
juju4 <juju4@users.noreply.github.com>
2017-01-06 16:45:03 -0500

    sync repositories

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,8 +7,14 @@ rolesdir=$(dirname $0)/..
 
 [ ! -d $rolesdir/geerlingguy.java ] && git clone https://github.com/geerlingguy/ansible-role-java.git $rolesdir/geerlingguy.java
 [ ! -d $rolesdir/ernestas-poskus.docker ] && git clone https://github.com/ernestas-poskus/ansible-role-docker.git $rolesdir/ernestas-poskus.docker
-[ ! -d $rolesdir/lxd ] && git clone https://github.com/juju4/ansible-lxd.git $rolesdir/lxd
-[ ! -d $rolesdir/lxdconfigure ] && git clone https://github.com/juju4/ansible-lxdconfigure.git $rolesdir/lxdconfigure
-[ ! -d $rolesdir/kitchen ] && git clone https://github.com/juju4/ansible-kitchen.git $rolesdir/kitchen
-[ ! -d $rolesdir/redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/redhat-epel
+[ ! -d $rolesdir/juju4.lxd ] && git clone https://github.com/juju4/ansible-lxd.git $rolesdir/juju4.lxd
+[ ! -d $rolesdir/juju4.lxdconfigure ] && git clone https://github.com/juju4/ansible-lxdconfigure.git $rolesdir/juju4.lxdconfigure
+[ ! -d $rolesdir/juju4.kitchen ] && git clone https://github.com/juju4/ansible-kitchen.git $rolesdir/juju4.kitchen
+[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+## galaxy naming: kitchen fails to transfer symlink folder
+#[ ! -e $rolesdir/juju4.jenkinsenv ] && ln -s ansible-jenkinsenv $rolesdir/juju4.jenkinsenv
+[ ! -e $rolesdir/juju4.jenkinsenv ] && cp -R $rolesdir/ansible-jenkinsenv $rolesdir/juju4.jenkinsenv
+
+## don't stop build on this script return code
+true
 
--- test/integration/default/serverspec/phpunit_spec.rb
+++ test/integration/default/serverspec/phpunit_spec.rb
@@ -6,7 +6,7 @@ set :backend, :exec
 describe file('/usr/local/bin/phpunit48') do
   it { should be_executable }
 end
-describe file('/usr/local/bin/phpunit54') do
+describe file('/usr/local/bin/phpunit') do
   it { should be_executable }
 end
 describe file('/usr/local/bin/phpmetrics') do
