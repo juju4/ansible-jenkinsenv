4ee7520f9813f8256bf862d28758044ed138dd5c
juju4 <juju4@users.noreply.github.com>
2020-07-26 17:34:20 -0400

    test/default serverspec: update for 20.04

--- test/integration/default/serverspec/packages_spec.rb
+++ test/integration/default/serverspec/packages_spec.rb
@@ -3,7 +3,10 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-describe package('python-virtualenv') do
+describe package('python3-virtualenv'), :if => (os[:family] == 'ubuntu' && os[:release] == '20.04') do
+  it { should be_installed }
+end
+describe package('python-virtualenv'), :if => !(os[:family] == 'ubuntu' && os[:release] == '20.04') do
   it { should be_installed }
 end
 describe package('git') do
